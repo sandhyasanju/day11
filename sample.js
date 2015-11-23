b8b8517ce6a4618b69a3a366cc6da0ee229cc3ed
sandhyasanju <p.sandhyarani20@gmail.com>
2015-11-23 12:31:25 +0530

    navigation page

--- sample.js
+++ sample.js
@@ -1,27 +1,35 @@
 function navigation() {
-  $(".icons").mouseenter(function() {
+  $(".icons").mouseenter(function()
+  {
     $(".text").show();
-    $(".text").mouseleave(function(){
-      $(".text").hide();
-      $("#home").click(function(){
-        $("#home1").toggle();
-        $("profile").click(function(){
-          $("#profile1").toggle();
-          $("#people").click(function(){
-            $("#profile1").toggle();
-            $(".table").toggle();
-            $("contact").click(function(){
-              $("#contact1").toggle();
-              $("#mail").click(function(){
-                $("#mail1").toggle();
-                $("#adress").click(function(){
-                  $("#adress1").toggle();
-                });
-              });
-            });
-          });
-        });
-      });
-    });
   });
-}
+  $(".text").mouseleave(function()
+    {
+      $(".text").hide();
+    });
+    $("#home").click(function()
+    {
+      $("#home1").toggle();
+    });
+    $("#profile").click(function()
+    {
+      $("#profile1").toggle();
+    });
+    $("#people").click(function()
+    {
+      $("#people1").toggle();
+      $(".table").toggle();
+     });
+     $("#contact").click(function()
+     {
+       $("#contact1").toggle();
+     });
+     $("#mail").click(function()
+     {
+       $("#mail1").toggle();
+     });
+     $("#adress").click(function()
+     {
+       $("#adress1").toggle();
+     });
+   }
